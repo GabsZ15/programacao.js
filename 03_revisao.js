48d4ed5ffc2c4d8d59dd6f4a74e33b10d1662ba2
GabsZ15 <gabrielantoniodecarvalho989@gmail.com>
2026-08-17 14:47:48 -0300

    revisão e acumulo de variáveis

---
+++ 03_revisao.js
@@ -0,0 +1,21 @@
+/*
+let aluguel = 1200.00;
+let condominio = 250.59;
+let internet = 85.99;
+let total = aluguel + condominio + internet;
+
+console.log("O total das contas é: " + total + " reais");
+
+
+
+
+let nome= "Gabriel";
+let sobrenome= "Carvalho";
+let nome_completo= nome + " " + sobrenome;
+console.log ("Seja bem-vindo(a): " +  nome_completo);
+
+
+
+let salario = 2000
+console.log ("Seu salário é: "  +  salario)   */
+
